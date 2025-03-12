8dcf0d0585a3582caeff3ac48814c306eba3a946
0525aa36 <0525aa36@gmail.com>
2025-03-12 19:00:32 +0900

    예약된 날짜 동그라미 수정정

--- app.py
+++ app.py
@@ -379,7 +379,7 @@ def my_reservations():
         match = matches_collection.find_one({'_id': ObjectId(res['match_id'])})
         if match:
             # creator_id로 예약자의 username 찾기
-            creator = users_collection.find_one({'_id': ObjectId(match['creator_id'])})
+            creator = users_collection.find_one({'_id': match['creator_id']})
             creator_name = creator['username'] if creator else "알 수 없음"
             
             reservations.append({
@@ -424,34 +424,35 @@ def api_reservations():
         return jsonify({'message': '사용자를 찾을 수 없습니다.'}), 404
 
     current_user_id = str(user['_id'])
-    my_reservations_cursor = reservations_collection.find({'user_id': current_user_id})
+    # user_id를 문자열이 아니라 ObjectId로 사용하여 조회
+    my_reservations_cursor = reservations_collection.find({'user_id': ObjectId(current_user_id)})
     reservations_data = []
     for res in my_reservations_cursor:
         match = matches_collection.find_one({'_id': ObjectId(res['match_id'])})
         if match:
-            # /get_matches와 동일한 방식으로 creator_name을 조회
             creator_name = "알 수 없음"
             creator_id = match.get('creator_id')
-            if ObjectId.is_valid(creator_id):
+            if ObjectId.is_valid(str(creator_id)):
                 creator = users_collection.find_one({'_id': ObjectId(creator_id)})
                 if creator:
                     creator_name = creator.get('username', '알 수 없음')
             reservations_data.append({
                 'reservation_id': str(res['_id']),
                 'match': {
-                    'date': match.get('date', ''),   # 날짜 필드 추가
+                    'date': match.get('date', ''),
                     'time_start': match.get('time_start', ''),
                     'time_end': match.get('time_end', ''),
                     'court_type': match.get('court_type', ''),
                     'memo': match.get('memo', ''),
                     'current_players': match.get('current_players', 0),
                     'max_players': match.get('max_players', 0),
-                    'creator_id': match.get('creator_id', ''),
+                    'creator_id': str(match.get('creator_id', '')),
                     'creator_name': creator_name,
                     '_id': str(match['_id'])
                 }
             })
     return jsonify({'reservations': reservations_data, 'current_user_id': current_user_id}), 200
+
 
 # 예약자 목록 조회
 @app.route('/player_list/<match_id>')
